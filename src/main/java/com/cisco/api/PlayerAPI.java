9d4b7443c44480956b6a5660b07ee493a6e0ecc0
sovisrushain <sovisrushain@gmail.com>
2024-06-10 20:27:33 +0530

    ERROR: roll back

--- src/main/java/com/cisco/api/PlayerAPI.java
+++ src/main/java/com/cisco/api/PlayerAPI.java
@@ -1,12 +1,11 @@
 package com.cisco.api;
 
-import com.cisco.model.Player;
-import com.cisco.repository.PlayerRepo;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,34 +14,46 @@ import java.util.List;
 public class PlayerAPI {
 
     private static final Logger log = LoggerFactory.getLogger(PlayerAPI.class);
-    private final PlayerRepo repo;
 
-    @GetMapping("/{id}")
-    public Player getPlayer(@PathVariable(value = "id") String id) {
-        log.info("Get player with id {}", id);
-        return repo.findById(id).get();
+    List<String> list = new ArrayList<>();
+
+    @PostMapping("/{data}")
+    public String postData(@PathVariable String data) {
+        list.add(data);
+        return "DONE!";
     }
 
     @GetMapping
-    public List<Player> getAllPlayers() {
-        log.info("Get all players");
-        return repo.findAll();
+    public List<String> getAll() {
+        return list;
     }
 
-    @PostMapping
-    public Player createPlayer(@RequestBody Player player) {
-        log.info("Create player with name {}", player.getName());
-        return repo.save(player);
-    }
-
-    @PutMapping
-    public Player updatePlayer(@RequestBody Player player) {
-        log.info("Update player with name {}", player.getName());
-        return repo.save(player);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deletePlayer(@PathVariable(value = "id") String id) {
-        repo.deleteById(id);
-    }
+//    @GetMapping("/{id}")
+//    public Player getPlayer(@PathVariable(value = "id") String id) {
+//        log.info("Get player with id {}", id);
+//        return repo.findById(id).get();
+//    }
+//
+//    @GetMapping
+//    public List<Player> getAllPlayers() {
+//        log.info("Get all players");
+//        return repo.findAll();
+//    }
+//
+//    @PostMapping
+//    public Player createPlayer(@RequestBody Player player) {
+//        log.info("Create player with name {}", player.getName());
+//        return repo.save(player);
+//    }
+//
+//    @PutMapping
+//    public Player updatePlayer(@RequestBody Player player) {
+//        log.info("Update player with name {}", player.getName());
+//        return repo.save(player);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public void deletePlayer(@PathVariable(value = "id") String id) {
+//        repo.deleteById(id);
+//    }
 }
--- src/main/java/com/cisco/repository/PlayerRepo.java
+++
@@ -1,7 +0,0 @@
-package com.cisco.repository;
-
-import com.cisco.model.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepo extends JpaRepository<Player, String> {
-}
