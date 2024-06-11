fabf3b410bd8e7e66ea9e61eca6e862a398f3987
sovisrushain <sovisrushain@gmail.com>
2024-06-11 11:46:43 +0530

    modify crud

--- src/main/java/com/cisco/api/PlayerAPI.java
+++ src/main/java/com/cisco/api/PlayerAPI.java
@@ -1,11 +1,12 @@
 package com.cisco.api;
 
+import com.cisco.model.Player;
+import com.cisco.repository.PlayerRepo;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -13,47 +14,35 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PlayerAPI {
 
+    private final PlayerRepo repo;
     private static final Logger log = LoggerFactory.getLogger(PlayerAPI.class);
 
-    List<String> list = new ArrayList<>();
-
-    @PostMapping("/{data}")
-    public String postData(@PathVariable String data) {
-        list.add(data);
-        return "DONE!";
+    @GetMapping("/{id}")
+    public Player getPlayer(@PathVariable(value = "id") String id) {
+        log.info("Get player with id {}", id);
+        return repo.findById(id).get();
     }
 
     @GetMapping
-    public List<String> getAll() {
-        return list;
+    public List<Player> getAllPlayers() {
+        log.info("Get all players");
+        return repo.findAll();
     }
 
-//    @GetMapping("/{id}")
-//    public Player getPlayer(@PathVariable(value = "id") String id) {
-//        log.info("Get player with id {}", id);
-//        return repo.findById(id).get();
-//    }
-//
-//    @GetMapping
-//    public List<Player> getAllPlayers() {
-//        log.info("Get all players");
-//        return repo.findAll();
-//    }
-//
-//    @PostMapping
-//    public Player createPlayer(@RequestBody Player player) {
-//        log.info("Create player with name {}", player.getName());
-//        return repo.save(player);
-//    }
-//
-//    @PutMapping
-//    public Player updatePlayer(@RequestBody Player player) {
-//        log.info("Update player with name {}", player.getName());
-//        return repo.save(player);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public void deletePlayer(@PathVariable(value = "id") String id) {
-//        repo.deleteById(id);
-//    }
+    @PostMapping
+    public Player createPlayer(@RequestBody Player player) {
+        log.info("Create player with name {}", player.getName());
+        return repo.save(player);
+    }
+
+    @PutMapping
+    public Player updatePlayer(@RequestBody Player player) {
+        log.info("Update player with name {}", player.getName());
+        return repo.save(player);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePlayer(@PathVariable(value = "id") String id) {
+        repo.deleteById(id);
+    }
 }
